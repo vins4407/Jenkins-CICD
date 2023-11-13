3463cbfc8aa62f70d540487e254a701433059bca
vins4407 <vkesherwani000@gmail.com>
2023-11-13 18:18:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>[ Vite + React ] App</h1>
+      <h1>Jenkins CICD AWS/Docker/Node</h1>
+
+      
+    </>
+  )
+}
+
+export default App
